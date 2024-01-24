f7a5a64289d704a9a5b34be47c568084aeee2390
cheolmin99 <cjfals8570@naver.com>
2024-01-24 16:50:52 +0900

    나누었던 클래스들을 context로 정의

--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -4,16 +4,14 @@ import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 import FakeYoutube from '../api/fakeYoutube';
 import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
     const { keyword } = useParams();
+    const { youtube } = useYoutubeApi();
     const { isLoading, error, data: videos } = useQuery({
         queryKey: ["videos", keyword],
-        queryFn: async () =>
-            {
-                const youtube = new FakeYoutube();
-                return youtube.search(keyword);
-            }
+        queryFn: async () => youtube.search(keyword)
             });
     return (
         <>

8698c5947ac36bcabfe1181b53e36cd319fed3fc
cheolmin99 <cjfals8570@naver.com>
2024-01-25 13:10:38 +0900

    2가지 버전으로 만들어 코드중복 피하기

--- youtube/src/context/YoutubeApiContext.jsx
+++ youtube/src/context/YoutubeApiContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useContext } from "react";
 import Youtube from "../api/youtube";
-import FakeYoutube from "../api/fakeYoutube";
+import YoutubeClient from "../api/youtubeClient";
+import FakeYoutubeClient from "../api/fakeYoutubeClient";
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new FakeYoutube(); //new Youtube();
+const client = new FakeYoutubeClient();
+//const client = new YoutubeClient();
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
     return (
--- youtube/src/pages/Videos.jsx
+++ youtube/src/pages/Videos.jsx
@@ -2,8 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube from '../api/youtube';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
